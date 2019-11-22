aca108290c70fdb3390912b172d7b007170f0580
yuanweimin <ywm00@qq.com>
2019-11-22 13:23:31 +0800

    add tuple example

---
+++ python-basic/tuple_example/example.py
@@ -0,0 +1,27 @@
+"""
+python data type:
+    1) Number
+    2) String
+    3) List
+    4) Tuple
+    5) Set
+    6) Dictionary
+
+example of Tuple blow
+"""
+
+
+def main():
+
+    # 元组创建
+    tup_a = ("aa", "bb", 'cc', 'dd')
+
+    # 元组迭代
+    for x in tup_a:
+        print(x)
+
+    # 元组不可修改
+
+
+if __name__ == '__main__':
+    main()
